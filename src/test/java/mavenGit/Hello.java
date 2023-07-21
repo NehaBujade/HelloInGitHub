c35b911c0330f5672b5a3f4edf43bbc569173055
nehab <nehab@Admin>
2023-07-21 12:55:16 +0530

    [Neha Bujade]First Commit

---
+++ src/test/java/mavenGit/Hello.java
@@ -0,0 +1,12 @@
+package mavenGit;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Hello {
+  @Test
+  public void firstMethod() {
+	 
+	  Reporter.log("Hello",true);
+	  
+  }
+}
